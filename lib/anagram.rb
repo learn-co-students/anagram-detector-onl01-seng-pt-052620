91fb98d451f23b0b56176ff1471f05bdd7d2896e
justinmcla <jwmclawhorn@gmail.com>
2020-06-11 14:49:32 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :query
+
+    def initialize(query)
+        @query = query
+    end
+
+    def match(list)
+        arr1 = @query.split('').sort
+        match_arr = []
+        list.each do |word|
+            if arr1 == word.split('').sort
+                match_arr << word
+            end
+        end
+        match_arr
+    end
+
+end
