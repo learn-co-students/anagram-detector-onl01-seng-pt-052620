c4d3d4d683a16de4b5e1fca1f5e815847a4df4e6
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-06-16 01:06:16 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+require "pry"
+class Anagram
+    attr_accessor :word 
+
+     def initialize(word)
+        @word = word 
+     end 
+
+     def match(array)
+      array.keep_if do |string|
+         word.split("").sort == string.split("").sort  
+      end
+   end
+end  
+
+
