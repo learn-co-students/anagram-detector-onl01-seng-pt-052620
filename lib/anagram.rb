a8788b0a4471e4000c2ee53feb72e714e0fa113e
bebofofum <bri.boehm@gmail.com>
2020-06-18 17:16:18 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word, :choices
+
+  def initialize(word)
+    @choices = []
+    @word = word
+  end
+
+  def match(choices)
+    match = []
+    main_word_array_sort = []
+    main_word_array_sort = @word.split(//).sort
+    # binding.pry
+    choices.each do |array_word|
+      word_split = [] 
+      word_split = array_word.split(//).sort
+      if main_word_array_sort == word_split
+        match << array_word
+      end
+    end
+    match
+  end
+
+
+
+
+end
