891d84cbbaed57748c0e370b6f0b1674c2960c2a
ndorazil <nathandorazil@live.com>
2020-06-23 22:51:07 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        @words = words
+        new_words = []
+        words.each do |word|
+            new_word = word.split('').sort.join('')
+            new_words << new_word
+        end
+        if new_words == words
+            @word
+        end
+    end
+end
