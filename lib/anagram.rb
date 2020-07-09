b6e9d02365222f411e6625d0746f2a5d400b8f08
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-09 20:56:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word_to_analyze
+
+  def initialize(word)
+    @word_to_analyze = word
+  end
+
+  def match(given_string)
+    given_string.find_all do |element|
+      (element.split("").sort) == (self.word_to_analyze.split("").sort)
+    end
+  end
+
+
+end
